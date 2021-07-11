ba2df175f96aedef47fe804a1b4d08dc272ad168
azaika <takaaza@gmail.com>
2021-07-12 01:11:24 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod ast;
+mod sld;
+
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammer); // synthesized by LALRPOP
+
+#[test]
+fn grammer() {
+    assert!(grammer::TermParser::new().parse("(x)").is_ok());
+    assert!(grammer::TermParser::new().parse("f(x, y, Z)").is_ok());
+
+    assert!(grammer::TermParser::new().parse("22").is_err());
+    assert!(grammer::TermParser::new().parse("(22)").is_err());
+    assert!(grammer::TermParser::new().parse("((((22))))").is_err());
+    assert!(grammer::TermParser::new().parse("((22)").is_err());
+}
+
+fn main() {}
